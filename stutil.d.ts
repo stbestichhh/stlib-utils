093d8de8db500ac7ae82300a084b9738af26530f
Kiril Yakymchuk <stbestich@gmail.com>
2024-05-09 11:46:09 +0300

    IsExists types

--- stutil.d.ts
+++ stutil.d.ts
@@ -1,19 +1,21 @@
 import { PathLike } from 'fs';
+import fs from 'fs';
+import node_path from 'path';
 
 // Utility: error
 
-export interface Options {
+export interface HandleErrorOptions {
   message?: string;
   throw?: boolean;
   toLog?: { path: PathLike; withStack?: boolean };
 }
 
 export function isError(error: unknown): boolean;
-export function handleErrorSync(error: unknown, options?: Options): void;
+export function handleErrorSync(error: unknown, options?: HandleErrorOptions): void;
 export function handleError(
   error: unknown,
   callback: () => void | Promise<void>,
-  options?: Options,
+  options?: HandleErrorOptions,
 ): Promise<void>;
 export function logErrorSync(
   error: unknown,
@@ -27,3 +29,18 @@ export function logError(
 ): Promise<void>;
 
 // Utility: fs
+
+export interface CreateOptions {
+  create: boolean;
+  recursive?: boolean;
+  content?: string;
+}
+
+export function isExistsSync(
+  path: PathLike,
+  options?: CreateOptions,
+): boolean;
+export function isExists(
+  path: PathLike,
+  options?: CreateOptions,
+): Promise<boolean>;
