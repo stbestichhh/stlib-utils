92bd79f5924d72e89777b16771f90d1bdc2d48d7
Kiril Yakymchuk <stbestich@gmail.com>
2024-05-14 15:56:57 +0300

    provided flexibility

--- lib/cli.ts
+++ lib/cli.ts
@@ -2,15 +2,35 @@ export const parseArgs = (argv: string[]) => {
   const options: { [key: string]: string } = {};
   const args = argv.slice(2);
 
-  args.forEach((arg) => {
-    if (arg.startsWith('--')) {
-      const [key, value] = arg.slice(2).split('=');
-      options[key] = value || '';
-    } else if (arg.startsWith('-')) {
-      const [key, value] = arg.slice(1).split('=');
-      options[key] = value || '';
+  for(let i = 0; i < args.length; i++) {
+    if(args[i].startsWith('--')) {
+      if(args[i + 1] && !args[i + 1].startsWith('-')) {
+        const [key] = args[i].slice(2).split('=');
+        const value = args[i + 1];
+        options[key] = value || '';
+      } else {
+        const [key, value, valuesp] = args[i].slice(2).split('=');
+        if(valuesp) {
+          options[key] = `${value}=${valuesp}` || '';
+        } else {
+          options[key] = value || '';
+        }
+      }
+    } else if(args[i].startsWith('-')) {
+      if(args[i + 1] && !args[i + 1].startsWith('-')) {
+        const [key] = args[i].slice(1).split('=');
+        const value = args[i + 1];
+        options[key] = value || '';
+      } else {
+        const [key, value, valuesp] = args[i].slice(1).split('=');
+        if(valuesp) {
+          options[key] = `${value}=${valuesp}` || ''
+        } else {
+          options[key] = value || '';
+        }
+      }
     }
-  });
+  }
 
   return options;
 };
