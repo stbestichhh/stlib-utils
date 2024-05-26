7e7cf871e2c6b600928bd44d88be2d2aa1b8306f
Kiril Yakymchuk <stbestich@gmail.com>
2024-05-26 14:08:18 +0300

    new config types

--- stutil.d.ts
+++ stutil.d.ts
@@ -63,17 +63,23 @@ export function isExists(
 
 // Utility: config
 
+export interface ConfigOptionsType {
+  force?: boolean;
+  alter?: boolean
+}
+
 export interface ConfigType {
   [key: string]: string | number;
 }
 
 export class Config {
-  constructor(path: PathLike, config: ConfigType);
+  constructor(path: PathLike, config: ConfigType, options?: ConfigOptionsType);
+  private initialize(options?: ConfigOptionsType): void
   public readonly path: PathLike;
   private config: ConfigType;
   read(): ConfigType;
-  static read(path: PathLike): Promise<{ [key: string]: string | number }>;
-  static readSync(path: PathLike): { [key: string]: string | number };
+  static read(path: PathLike): Promise<ConfigType>;
+  static readSync(path: PathLike): ConfigType;
   get(key: string): string | number;
   static get(key: string, path: PathLike): Promise<string | number>;
   static getSync(key: string, path: PathLike): string | number;
