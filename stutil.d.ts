86a1bc66b904266e01ebae9686fa132f5b8fe944
Kiril Yakymchuk <stbestich@gmail.com>
2024-08-04 12:40:28 +0300

    removed unused in config functions

--- stutil.d.ts
+++ stutil.d.ts
@@ -1,66 +1,5 @@
 import { PathLike } from 'fs';
 
-// Utility: error
-
-export interface HandleErrorOptions {
-  message?: string;
-  throw?: boolean;
-  toLog?: { path: PathLike; withStack?: boolean };
-}
-
-export function isError(error: unknown): boolean;
-export function handleErrorSync(
-  error: unknown,
-  options?: HandleErrorOptions,
-): { handledError: unknown };
-export function handleError<T>(
-  error: unknown,
-  callback: () => T | Promise<T>,
-  options?: HandleErrorOptions,
-): Promise<{ callbackResult: T | undefined; handledError: unknown }>;
-export function logErrorSync(
-  error: unknown,
-  path: PathLike,
-  stack?: boolean,
-): void;
-export function logError(
-  error: unknown,
-  path: PathLike,
-  stack?: boolean,
-): Promise<void>;
-
-export abstract class AbstractError extends Error {
-  abstract readonly code: number;
-  readonly message: string;
-  readonly name: string;
-  readonly options?: { [key: string]: string | number };
-  protected constructor(
-    message: string,
-    options?: { [key: string]: string | number },
-  );
-  serialize(): {
-    name: string;
-    message: string;
-    code: number;
-    options: { [key: string]: string | number } | undefined;
-    stack: string | undefined;
-  };
-}
-
-// Utility: fs
-
-export interface CreateOptions {
-  create: boolean;
-  recursive?: boolean;
-  content?: string;
-}
-
-export function isExistsSync(path: PathLike, options?: CreateOptions): boolean;
-export function isExists(
-  path: PathLike,
-  options?: CreateOptions,
-): Promise<boolean>;
-
 // Utility: config
 
 export interface ConfigOptionsType {
@@ -89,20 +28,3 @@ export class Config {
   static write(path: PathLike, config: ConfigType): Promise<void>;
   static writeSync(path: PathLike, config: ConfigType): void;
 }
-
-// Utility: cli
-
-export interface OptionsType {
-  [key: string]: string | boolean;
-}
-
-export function parseArgs(argv: string[]): OptionsType;
-export const options: OptionsType;
-
-// Utility: cancellable
-
-export function cancellable(
-  callback: (...args: unknown[]) => Promise<unknown>,
-  args: unknown[],
-  sleepTime?: number,
-): void;
--- stutil.ts
+++ stutil.ts
@@ -1,5 +1,1 @@
-export * from './lib/error';
-export * from './lib/fs';
 export * from './lib/config';
-export * from './lib/cli';
-export * from './lib/cancellable';
